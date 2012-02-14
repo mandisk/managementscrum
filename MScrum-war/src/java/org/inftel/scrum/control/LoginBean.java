7a99c0a2078a815dd7e028aac9b1f16be9ffa1dc
jorge.pb1987@gmail.com <jorge.pb1987@gmail.com@3ac73a75-b84a-611e-cb85-695edb345667>
2012-02-14 18:05:16 +0000

    Messages properties. LoginBean de session.

--- MScrum-war/src/java/org/inftel/scrum/control/LoginBean.java
+++ MScrum-war/src/java/org/inftel/scrum/control/LoginBean.java
@@ -7,7 +7,7 @@ package org.inftel.scrum.control;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import org.inftel.scrum.bean.LoginBaseBean;
 import org.inftel.scrum.ejb.UserFacade;
@@ -19,15 +19,25 @@ import org.inftel.scrum.util.Util;
  * @author Jorge
  */
 @ManagedBean
-@RequestScoped
+@SessionScoped
 public class LoginBean extends LoginBaseBean{
     @EJB
     private UserFacade userFacade;
+    private User user;
 
     /**
      * Creates a new instance of LoginBean
      */
     public LoginBean() {
+        user = null;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
     
     public String doLogin() {
@@ -38,10 +48,9 @@ public class LoginBean extends LoginBaseBean{
             FacesContext.getCurrentInstance().addMessage(null, msg);
             return null;
         }
-        User u = userFacade.findByEmail(email);
-        if (u != null) {
-            if (u.getPassword().trim().equals(Util.md5(password.trim()).trim())) {
-                FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("user", u);
+        user = userFacade.findByEmail(email);
+        if (user != null) {
+            if (user.getPassword().trim().equals(Util.md5(password.trim()).trim())) {
                 return "main";
             }
             msg = new FacesMessage("Wrong password");
